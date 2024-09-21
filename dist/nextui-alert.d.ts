3932296bb1ce3245b8ba3d97d563fa953718820a
Zain Adam <zainadam.id@gmail.com>
2024-09-22 04:45:02 +0800

    Rebuild dist

--- dist/nextui-alert.d.ts
+++ dist/nextui-alert.d.ts
@@ -1,2 +1,2 @@
 import type NextuiAlertProps from './@types/nextui-alert-props';
-export default function NextuiAlert({ severity, variant, color: _color, startContent: _startContent, title, children, onClose, endContent, ...restProps }: NextuiAlertProps): import("react/jsx-runtime").JSX.Element;
+export default function NextuiAlert({ severity, variant, shadow, color: _color, startContent: _startContent, title, children, onClose, endContent, ...restProps }: NextuiAlertProps): import("react/jsx-runtime").JSX.Element;
