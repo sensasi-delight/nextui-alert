ba0670a1c6a4bbf4fe53654583813795ffc4130a
sensasi-delight <sensasi-delight@users.noreply.github.com>
2024-12-15 13:33:46 +0000

    Auto build files

--- dist/functions/get-start-content-cn-value.d.ts
+++
@@ -1,2 +0,0 @@
-import ThemeColorType from '../@types/theme-color';
-export declare function getStartContentCnValue(color: ThemeColorType): "text-primary" | "text-secondary" | "text-success" | "text-warning" | "text-danger";
